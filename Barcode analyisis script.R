efe4ba0a6853b28de0c4892e441db88f17b4db53
unknown <you@example.com>
2019-01-30 16:30:06 -0500

    Final script, .rproj folder, and .gitignore file

--- Barcode analyisis script.R
+++ Barcode analyisis script.R
@@ -1,7 +1,48 @@
 library(sangerseqR)
+setwd("C:/Users/Adell/Documents/Barcode_Analysis")
 
-ITS<-read.abif("./Data/DNA_Barcoding/1Ipos_F_P1815443_064.ab1") # Read
+ITS<-read.abif("./Data/1Ipos_F_P1815443_064.ab1") # Read
 
-ITSseq <- sangerseq(ITS) # Extract
+ITSseq <- sangerseq(ITS) # Extract sequence 
 
-SeqX<-makeBaseCalls(ITSseq) # Call 
+SeqX<-makeBaseCalls(ITSseq) # Call sequence
+
+##isolate primary sequence for first file and make into FASTA format
+
+PseqX <- SeqX@primarySeq
+
+PseqX2 <- gsub('^', '>1Ipos_F_P1815443_064.ab1\n', PseqX)
+
+
+##cycle through multiple .ab1 files in DATA folder
+
+#create a list of files as directory locations
+seqFiles <- list.files('./Data/', full.names = T)
+
+#load quality check file and subset to files NOT passing quality check
+qCheck <- read.csv('./Data/BarcodePlateStats.csv')
+removeSeq <- subset(qCheck, Ok == 'FALSE')
+
+# cycle through files, construct FASTA file and insert into vector
+Fseqs <- character(length(1:33))
+myPos <- 1
+
+for (i in seqFiles){
+  myHeader <- gsub('.*Data/', '', i)
+  if (myHeader %in% removeSeq$Chromatogram){
+    next
+  }
+  rSeq <- read.abif(i)
+  seqITS <- sangerseq(rSeq)
+  BCseq <- makeBaseCalls(seqITS)
+  Pseq <- BCseq@primarySeq
+  
+  Pseq2 <- gsub('^', '\n', Pseq)
+  Pseq2_2 <- gsub('$', '\n', Pseq2)
+  Pseq3 <- gsub('^', myHeader, Pseq2_2)
+  Pseq4 <- gsub('^', '>', Pseq3)
+  Fseqs[myPos] <- Pseq4
+  myPos = myPos + 1
+}
+
+cat(Fseqs)
